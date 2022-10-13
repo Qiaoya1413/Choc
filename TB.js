f27e6380beebdef236386af54b515c2fa66bd9c7
Qiao <101532293+Qiaoya1413@users.noreply.github.com>
2022-10-13 14:35:58 +0800

    Update TB.js

--- TB.js
+++ TB.js
@@ -1,6 +1,5 @@
 /*
-https://push.tapbots.com/tweetbot/5/verify_subscription url script-response-body https://raw.githubusercontent.com/Ssiswent/QuanX/main/tweetbot.js
-
+https://push.tapbots.com/tweetbot/5/verify_subscription url script-response-body https://raw.githubusercontent.com/Qiaoya1413/Aco/main/TB.js
 hostname=*.tapbots.com
 */
 
