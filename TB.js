0537321b8a9414670706a93c1c8e2a6411378856
Qiao <101532293+Qiaoya1413@users.noreply.github.com>
2022-10-13 14:35:05 +0800

    Create TB.js

---
+++ TB.js
@@ -0,0 +1,12 @@
+/*
+https://push.tapbots.com/tweetbot/5/verify_subscription url script-response-body https://raw.githubusercontent.com/Ssiswent/QuanX/main/tweetbot.js
+
+hostname=*.tapbots.com
+*/
+
+let obj = JSON.parse($response.body);
+
+obj["expires_at"]= 3476097520,
+obj["can_trial"]= true,
+
+$done({body: JSON.stringify(obj)});
