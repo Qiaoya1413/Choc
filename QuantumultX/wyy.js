fcb11b1cbe0279ac717a8a35a54c412a783726f0
Qiao <101532293+Qiaoya1413@users.noreply.github.com>
2023-12-01 11:09:24 +0800

    Update wyy.js

--- QuantumultX/wyy.js
+++ QuantumultX/wyy.js
@@ -2,20 +2,24 @@
 搬搬党自用
 
 [rewrite_local]
-# 播放器皮肤
+# 播放器会员皮肤
 ^https:\/\/interface3\.music\.163\.com\/eapi\/playermode\/ url script-request-header https://raw.githubusercontent.com/Qiaoya1413/Choc/main/QuantumultX/wyy.js
 
-# 搜索结果列表
-^https:\/\/interface3\.music\.163\.com\/eapi\/search\/complex\/page url script-request-header https://raw.githubusercontent.com/Qiaoya1413/Choc/main/QuantumultX/wyy.js
+# 搜索结果会员歌曲
+^https:\/\/interface3\.music\.163\.com\/eapi\/search\/complex\/(page|rec\/song\/get) url script-request-header https://raw.githubusercontent.com/Qiaoya1413/Choc/main/QuantumultX/wyy.js
 
-# 会员歌曲
-^https:\/\/interface3\.music\.163\.com\/eapi\/song\/ url script-request-header https://raw.githubusercontent.com/Qiaoya1413/Choc/main/QuantumultX/wyy.js
+# 播放器会员歌曲
+^https:\/\/interface3\.music\.163\.com\/eapi\/song\/(chorus|enhance\/|type\/detail\/get) url script-request-header https://raw.githubusercontent.com/Qiaoya1413/Choc/main/QuantumultX/wyy.js
+^https:\/\/interface3\.music\.163\.com\/eapi\/(v1\/artist\/top\/song|v3\/discovery\/recommend\/songs) url script-request-header https://raw.githubusercontent.com/Qiaoya1413/Choc/main/QuantumultX/wyy.js
 
-# 会员歌曲列表
+# 侧边栏会员等级
+^https:\/\/interface3\.music\.163\.com\/eapi\/vipnewcenter\/app\/resource\/newaccountpage url script-request-header https://raw.githubusercontent.com/Qiaoya1413/Choc/main/QuantumultX/wyy.js
+
+# 首页歌单会员歌曲
 ^https?:\/\/interface3\.music\.163\.com\/eapi\/(homepage\/|v6\/)?playlist\/ url script-request-header https://raw.githubusercontent.com/Qiaoya1413/Choc/main/QuantumultX/wyy.js
 
-# 音质切换
-^https?:\/\/interface3\.music\.163\.com\/eapi\/vipauth\/app\/auth\/ url script-request-header https://raw.githubusercontent.com/Qiaoya1413/Choc/main/QuantumultX/wyy.js 
+# 会员认证
+^https?:\/\/interface3\.music\.163\.com\/eapi\/vipauth\/app\/auth\/(soundquality\/)?query url script-request-header https://raw.githubusercontent.com/Qiaoya1413/Choc/main/QuantumultX/wyy.js
 
 [mitm] 
 hostname = interface*.music.163.com
